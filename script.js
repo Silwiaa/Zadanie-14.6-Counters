0308c2f83d8a246674a2686a948736bc67699f84
Sylwia Krzymowska <s.krzymowska@gmail.com>
2018-05-31 20:26:04 +0200

    finish

--- script.js
+++ script.js
@@ -8,24 +8,25 @@ var Counter = React.createClass({
     increment: function() {
         this.setState({
             counter: this.state.counter + 1
-        });
+        })
     },
 
     decrement: function() {
         this.setState({
             counter: this.state.counter - 1
-        });
+        })
     },
     
     render: function() {
         return React.createElement('div', {onClick: this.increment},
             React.createElement('button', {}, 'Increment ' + this.state.counter)
         ),
-        React.createElement('div', {onClick: this:Decrement},
-            React.createElement('button', {}, 'Decrement ' + this.state.counter)
+        React.createElement('div', {onClick: this.decrement},
+           React.createElement('button', {}, 'Decrement ' + this.state.counter)
         )
     }
 });
+
 
 var element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
